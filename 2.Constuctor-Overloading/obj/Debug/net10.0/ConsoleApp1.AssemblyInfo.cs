c1e1e143caaf8582fb157d477b6dd7ca25a34798
JanithFX <janithfx2021@gmail.com>
2026-04-29 13:12:13 +0530

    encapsulation

--- 2.Constuctor-Overloading/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ 2.Constuctor-Overloading/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa10768b21853bf3180aaa7cfc6ff337e8fa189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98429abb733e59a737f796b55b74f147e3ed0a71")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 3.Encapsulation-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+       BankAccount account = new BankAccount();
+
+       account.Balance = 1000.0;
+       Console.WriteLine("Current Balance: " + account.Balance);
+    }
+}
